cf3621d1e378fb5bed7fe7ffb87c920143acb457
Iaannn06 <alfianazad05@gmail.com>
2024-04-01 15:36:25 +0700

    membuat objek untuk menyimpan alamat desa

--- struct2[/struct2[.cpp
+++ struct2[/struct2[.cpp
@@ -2,12 +2,30 @@
 using namespace std;
 
 struct DetailAlamat {
-	string desa;
-	string kota;
+	string Desa;
+	string Kota;
 };
-struct mahasiswa {
+struct Mahasiswa {
 	string nim;
 	string nama;
 	DetailAlamat alamat;
 	int umur;
 };
+int main() {
+	Mahasiswa mhs;
+	cout << "Nomor Mahasiswa :";
+	cin >> mhs.nim;
+	cout << "Nama Mahasiswa : ";
+	cin >> mhs.nama;
+	cout << "Alamat Mahasiswa :";
+	cout << "\t Nama Desa : ";
+	cin >> mhs.alamat.Desa;
+	cout << "Umur Mahasiswa :";
+	cin >> mhs.umur;
+
+	cout << endl;
+	cout << "\n Nim : " << mhs.nim;
+	cout << "\n Nama : " << mhs.nama;
+	cout << "\n Alamat : " << mhs.alamat;
+	cout << "\n Umur : " << mhs.umur;
+}
