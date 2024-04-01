1964d77082d4f284eb3ca7d9b69022894dccb4b0
Iaannn06 <alfianazad05@gmail.com>
2024-04-01 16:45:35 +0700

    menambahkan getline pada variabel nim

---
+++ struct3/struct3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+	string Desa;
+	string Kota;
+};
+struct Mahasiswa {
+	string nim;
+	string nama;
+	DetailAlamat alamat;
+	int umur;
+};
+int main() {
+	Mahasiswa mhs;
+	cout << "Nomor Mahasiswa :";
+	cin >> mhs.nim;
+	getline(cin, mhs.nim);
+	cout << "Nama Mahasiswa : ";
+	cin >> mhs.nama;
+	cout << "Alamat Mahasiswa :" << endl;
+	cout << "\t Nama Desa : ";
+	cin >> mhs.alamat.Desa;
+	cout << "\t Nama Kota : ";
+	cin >> mhs.alamat.Kota;
+	cout << "Umur Mahasiswa :";
+	cin >> mhs.umur;
+
+	cout << endl;
+	cout << "\n Nim : " << mhs.nim;
+	cout << "\n Nama : " << mhs.nama;
+	cout << "\t \n Desa : " << mhs.alamat.Desa;
+	cout << "\t \n Kota : " << mhs.alamat.Kota;
+	cout << "\n Umur : " << mhs.umur;
+}
