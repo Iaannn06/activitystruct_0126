c50253ca7f3a49c89258cc4d5aacca742c365af1
Iaannn06 <alfianazad05@gmail.com>
2024-04-01 15:54:42 +0700

    memanggil variable alamat desa

--- struct2[/struct2[.cpp
+++ struct2[/struct2[.cpp
@@ -19,17 +19,12 @@ int main() {
 	cin >> mhs.nama;
 	cout << "Alamat Mahasiswa :";
 	cout << "\t Nama Desa : ";
-	cin >> mhs.alamat.Desa;
-	cout << "\t Nama Kota : ";
-	cin >> mhs.alamat.Kota;
 	cout << "Umur Mahasiswa :";
 	cin >> mhs.umur;
 
 	cout << endl;
 	cout << "\n Nim : " << mhs.nim;
 	cout << "\n Nama : " << mhs.nama;
-	cout << "\n Alamat : ";
-	cout << "\n Desa :" << mhs.alamat.Desa;
-	cout << "\n Kota :" << mhs.alamat.Kota;
+	cout << "\n Alamat : " << mhs.alamat;
 	cout << "\n Umur : " << mhs.umur;
 }
